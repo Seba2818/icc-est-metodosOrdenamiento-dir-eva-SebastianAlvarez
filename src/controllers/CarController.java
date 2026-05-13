927e9e2ca977c602d917188152a7aee3d4e79c73
Your Name <you@example.com>
2026-05-13 13:16:15 +0000

    upd

--- src/controllers/CarController.java
+++ src/controllers/CarController.java
@@ -4,6 +4,13 @@ import models.Car;
 
 public class CarController {
   
-  public void sortByName(Car[] cars[]);
-  
+  public void sortByName(Car[] cars) {
+    for (int i = 0; i < cars.length-1 ; i++){
+      boolean swap = false;
+      for (int j = 0; j< cars.length; j++) {
+        int aux =
+      }
+    }
+  }
+
 }
